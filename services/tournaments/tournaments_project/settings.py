40bda53f71b6c132310890aa201378e08cbb6021
David Garizado <dgarizad@c3r9s4.42malaga.com>
2024-11-21 17:22:15 +0100

    preparing for dg branch

--- services/tournaments/tournaments_project/settings.py
+++ services/tournaments/tournaments_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o-lvlvmrx=iw=256ov)_a!iocrv^u9%isbd9$*yfwz^ej)m1ok'
+SECRET_KEY = 'django-insecure-5y#pq!0q&@siq$bk30!iq)ke$nw&p#bilw%l$%0u(#)gg7*)zf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'tournaments_app',
 ]
 
 MIDDLEWARE = [
